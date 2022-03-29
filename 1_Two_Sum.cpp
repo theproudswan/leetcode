6f77d237954054db926c44b7b3283a8a57195de4
Ivan Lebedev <40741270+theproudswan@users.noreply.github.com>
2022-03-29 20:13:16 +0300

    Update 1_Two_Sum.cpp

--- 1_Two_Sum.cpp
+++ 1_Two_Sum.cpp
@@ -10,19 +10,13 @@ vector<int> twoSum(vector<int>& nums, int target) {
 
     for (int i = 0; i < nums.size(); ++i){
         pos_val.insert({i, nums[i]});
-        cout << "Inserted " << nums[i] << " on position " << i << endl;
-        cout << "Trying to find " << target - nums[i] << endl;
-        // unordered_map<int, int>::iterator it = pos_val.find(target - nums[i]);
+
         for (auto it = begin(pos_val); it != end(pos_val); it++){
             if ((it->second == target - nums[i]) && (it->first != i)){
                 return {i , it->first};
             }
         }
-        // if (it != end(pos_val)) {
-        //     cout << "Found " << it->second << " on position " << it->first << endl;
 
-        //     return {i , it->first};
-        // }
     }
     return {-1, -1};
 }
