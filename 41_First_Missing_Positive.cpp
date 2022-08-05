47b1bb9dbc32175bf52aca668f7c367eed66d5a8
Ivan Lebedev <40741270+theproudswan@users.noreply.github.com>
2022-08-05 18:45:01 +0300

    Create 41_First_Missing_Positive.cpp

---
+++ 41_First_Missing_Positive.cpp
@@ -0,0 +1,14 @@
+// O(n)    
+
+int firstMissingPositive(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        if (find(begin(nums), end(nums), 1) == end(nums)) return 1;
+        for (size_t i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] > 0) {
+                if (nums[i + 1] - nums[i] > 1) {
+                    return nums[i] + 1;
+                }
+            }
+        }
+        return nums[nums.size() - 1] + 1;
+    }
